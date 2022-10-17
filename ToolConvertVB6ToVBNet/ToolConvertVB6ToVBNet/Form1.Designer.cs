94769a4d66080a1ecd953d47eb9ef84445b92c1f
tuan-vq <tuan-vq@fujinet.net>
2022-10-17 16:15:45 +0700

    commit src

--- ToolConvertVB6ToVBNet/ToolConvertVB6ToVBNet/Form1.Designer.cs
+++ ToolConvertVB6ToVBNet/ToolConvertVB6ToVBNet/Form1.Designer.cs
@@ -28,13 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panLeft = new System.Windows.Forms.Panel();
+            this.progressBarLoadDir = new System.Windows.Forms.ProgressBar();
+            this.treeViewDir = new System.Windows.Forms.TreeView();
+            this.btnDirectoryPath = new System.Windows.Forms.Button();
+            this.txtDirectoryPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panLeft.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Directory";
+            // 
+            // panLeft
+            // 
+            this.panLeft.Controls.Add(this.progressBarLoadDir);
+            this.panLeft.Controls.Add(this.treeViewDir);
+            this.panLeft.Controls.Add(this.btnDirectoryPath);
+            this.panLeft.Controls.Add(this.txtDirectoryPath);
+            this.panLeft.Controls.Add(this.label1);
+            this.panLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panLeft.Location = new System.Drawing.Point(0, 0);
+            this.panLeft.Name = "panLeft";
+            this.panLeft.Size = new System.Drawing.Size(321, 450);
+            this.panLeft.TabIndex = 1;
+            // 
+            // progressBarLoadDir
+            // 
+            this.progressBarLoadDir.Location = new System.Drawing.Point(6, 420);
+            this.progressBarLoadDir.Name = "progressBarLoadDir";
+            this.progressBarLoadDir.Size = new System.Drawing.Size(306, 23);
+            this.progressBarLoadDir.TabIndex = 4;
+            // 
+            // treeViewDir
+            // 
+            this.treeViewDir.Location = new System.Drawing.Point(6, 32);
+            this.treeViewDir.Name = "treeViewDir";
+            this.treeViewDir.Size = new System.Drawing.Size(306, 380);
+            this.treeViewDir.TabIndex = 3;
+            this.treeViewDir.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeViewDir_NodeMouseDoubleClick);
+            // 
+            // btnDirectoryPath
+            // 
+            this.btnDirectoryPath.Location = new System.Drawing.Point(289, 5);
+            this.btnDirectoryPath.Name = "btnDirectoryPath";
+            this.btnDirectoryPath.Size = new System.Drawing.Size(23, 23);
+            this.btnDirectoryPath.TabIndex = 2;
+            this.btnDirectoryPath.Text = "...";
+            this.btnDirectoryPath.UseVisualStyleBackColor = true;
+            this.btnDirectoryPath.Click += new System.EventHandler(this.btnDirectoryPath_Click);
+            // 
+            // txtDirectoryPath
+            // 
+            this.txtDirectoryPath.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.txtDirectoryPath.Location = new System.Drawing.Point(111, 6);
+            this.txtDirectoryPath.Name = "txtDirectoryPath";
+            this.txtDirectoryPath.Size = new System.Drawing.Size(171, 21);
+            this.txtDirectoryPath.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Controls.Add(this.txtFolderPath);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(321, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(140, 34);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(108, 22);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.txtFolderPath.Location = new System.Drawing.Point(97, 7);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(151, 21);
+            this.txtFolderPath.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panLeft);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panLeft.ResumeLayout(false);
+            this.panLeft.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panLeft;
+        private System.Windows.Forms.Button btnDirectoryPath;
+        private System.Windows.Forms.TextBox txtDirectoryPath;
+        private System.Windows.Forms.TreeView treeViewDir;
+        private System.Windows.Forms.ProgressBar progressBarLoadDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
     }
 }
 
--- ToolConvertVB6ToVBNet/ToolConvertVB6ToVBNet/Form1.cs
+++ ToolConvertVB6ToVBNet/ToolConvertVB6ToVBNet/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,174 @@ namespace ToolConvertVB6ToVBNet
 {
     public partial class Form1 : Form
     {
+        private string strFullPathSelect;
+
+        private string[] lstFind;
+
+        readonly string VB_TEXTBOX = "VB.TextBox";
+
         public Form1()
         {
             InitializeComponent();
+
+            lstFind = new string[] {
+                "imMask7Ctl.imMask","imNumber7Ctl.imNumber","imText6Ctl.imText", "FPSpreadADO.fpSpread",
+                "imText7Ctl.imText", "imTime7Ctl.imTime","imCalendar7Ctl.imCalendar" };
+        }
+
+        private void btnDirectoryPath_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.SelectedPath = txtDirectoryPath.Text;
+            DialogResult drResult = folderBrowserDialog.ShowDialog();
+            if (drResult == DialogResult.OK)
+            {
+                txtDirectoryPath.Text = folderBrowserDialog.SelectedPath;
+
+                // Setting Inital Value of Progress Bar
+                progressBarLoadDir.Value = 0;
+                // Clear All Nodes if Already Exists
+                treeViewDir.Nodes.Clear();
+                LoadDirectory(txtDirectoryPath.Text);
+            }
+        }
+
+        public void LoadDirectory(string Dir)
+        {
+            DirectoryInfo di = new DirectoryInfo(Dir);
+            //Setting ProgressBar Maximum Value
+            progressBarLoadDir.Maximum = Directory.GetFiles(Dir, "*.*", SearchOption.AllDirectories).Length + Directory.GetDirectories(Dir, "**", SearchOption.AllDirectories).Length;
+            TreeNode tds = treeViewDir.Nodes.Add(di.Name);
+            tds.Tag = di.FullName;
+            tds.StateImageIndex = 0;
+            LoadFiles(Dir, tds);
+            LoadSubDirectories(Dir, tds);
+        }
+
+        private void LoadFiles(string dir, TreeNode td)
+        {
+            string[] Files = Directory.GetFiles(dir, "*.*");
+            // Loop through them to see files
+            foreach (string file in Files)
+            {
+                FileInfo fi = new FileInfo(file);
+                TreeNode tds = td.Nodes.Add(fi.Name);
+                tds.Tag = fi.FullName;
+                tds.StateImageIndex = 1;
+                UpdateProgress();
+            }
+        }
+
+        private void LoadSubDirectories(string dir, TreeNode td)
+        {
+            // Get all subdirectories
+            string[] subdirectoryEntries = Directory.GetDirectories(dir);
+            // Loop through them to see if they have any other subdirectories
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                DirectoryInfo di = new DirectoryInfo(subdirectory);
+                TreeNode tds = td.Nodes.Add(di.Name);
+                tds.StateImageIndex = 0;
+                tds.Tag = di.FullName;
+                LoadFiles(subdirectory, tds);
+                LoadSubDirectories(subdirectory, tds);
+                UpdateProgress();
+            }
+        }
+
+        private void UpdateProgress()
+        {
+            if (progressBarLoadDir.Value < progressBarLoadDir.Maximum)
+            {
+                progressBarLoadDir.Value++;
+                int percent = (int)(((double)progressBarLoadDir.Value / (double)progressBarLoadDir.Maximum) * 100);
+                progressBarLoadDir.CreateGraphics().DrawString(percent.ToString() + "%", new Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBarLoadDir.Width / 2 - 10, progressBarLoadDir.Height / 2 - 7));
+                Application.DoEvents();
+            }
+        }
+
+        private void treeViewDir_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (!e.Node.Text.Contains("."))
+            {
+                txtFolderPath.Text = e.Node.Text;
+                strFullPathSelect = e.Node.Tag.ToString();
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(strFullPathSelect))
+                {
+                    // Setting Inital Value of Progress Bar
+                    progressBarLoadDir.Value = 0;
+
+                    //Setting ProgressBar Maximum Value
+                    progressBarLoadDir.Maximum = Directory.GetFiles(strFullPathSelect, "*.*", SearchOption.AllDirectories).Length + Directory.GetDirectories(strFullPathSelect, "**", SearchOption.AllDirectories).Length;
+
+                    string[] Files = Directory.GetFiles(strFullPathSelect, "*.*");
+                    foreach (string file in Files)
+                    {
+                        string fileBk = String.Empty;
+                        if (file.LastIndexOf(".frm") != -1 && file.LastIndexOf(".frm-bk") == -1)
+                        {
+                            fileBk = file.Replace(".frm", ".frm-bk");
+                            if (File.Exists(fileBk))
+                            {
+                                File.Delete(fileBk);
+                            }
+                            File.Copy(file, fileBk);
+
+                            readFileAndChange(file, 0);
+                        }
+                        if (file.LastIndexOf(".vbp") != -1 && file.LastIndexOf(".vbp-bk") == -1)
+                        {
+                            fileBk = file.Replace(".vbp", ".vbp-bk");
+                            if (File.Exists(fileBk))
+                            {
+                                File.Delete(fileBk);
+                            }
+                            File.Copy(file, fileBk);
+
+                            readFileAndChange(file, 1);
+                        }
+                        UpdateProgress();
+                    }
+                }
+            }
+            catch (IOException io)
+            {
+                MessageBox.Show("Conver is IOException: " + io.Message, "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void readFileAndChange(string path, int mode)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding(932));
+            String[] rows = Regex.Split(sr.ReadToEnd(), "\r\n");
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter(
+                new FileStream(path, FileMode.Open, FileAccess.ReadWrite), Encoding.GetEncoding(932));
+            for (int i = 0; i < rows.Length; i++)
+            {
+                foreach (string txtFind in lstFind)
+                {
+                    if (mode == 0 && rows[i].Contains(txtFind))
+                    {
+                        rows[i] = rows[i].Replace(txtFind, VB_TEXTBOX);
+                    }
+                }
+
+                if (mode == 1 && rows[i].LastIndexOf(".ocx") != -1)
+                {
+                    rows[i] = "\'" + rows[i];
+                }
+
+                sw.WriteLine(rows[i]);
+            }
+            sw.Close();
         }
     }
 }
